b880823e039ac5ee2975be90e6752dff44717acb
NSRipani <nicor.33@live.com>
2024-07-01 15:29:16 -0300

    viendo como funciona los archivos '.css'

--- E-commerce_React/src/componentes/navBar/navBar.jsx
+++ E-commerce_React/src/componentes/navBar/navBar.jsx
@@ -2,9 +2,9 @@ import Carro from "../cardCompras/iconoCart"
 
 const NavBar = () => {
     return (
-        <nav>
-            <h1>TITULO DEL E-COMMERCE</h1>
-            <div>
+        <nav className="navbar-conteiner">
+            <h1 className="navbar__title">TITULO DEL E-COMMERCE</h1>
+            <div className="navbar__links">
                 <a href="">ENLACE 1</a>
                 <a href="">ENLACE 2</a>
                 <a href="">ENLACE 3</a>
