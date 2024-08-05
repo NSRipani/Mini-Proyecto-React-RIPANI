ac5e4dedb57b3eedf7a04760f6f3d78bedf0f355
NSRipani <nicor.33@live.com>
2024-08-05 14:38:57 -0300

    reestablecido al estado 'pre-entrega 3'

--- E-commerce_React/src/component/context/context.jsx
+++ E-commerce_React/src/component/context/context.jsx
@@ -3,14 +3,9 @@ import {initializeApp} from 'firebase/app'
 import {getFirestore,collection, addDoc} from 'firebase/firestore'
 
 // Aquí se debe ingresar la configuración de firebase
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey, 
-    authDomain: process.env.REACT_APP_aauthDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
-}
+// const firebaseConfig = {
+//     ...
+// }
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
